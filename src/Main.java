84b61e1412e9097704a7949f635e4b2a02c79940
MatthieuMosch <mmosch@gmail.com>
2025-05-30 20:31:35 +0200

    start huiswerk

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Appeltaart:\n");
-        ApplePieRecipe applePieRecipe = new ApplePieRecipe();
-        applePieRecipe.printRecipe();
-        System.out.println("\nBananensap:\n");
-        BananaJuiceRecipe bananaJuiceRecipe = new BananaJuiceRecipe();
-        bananaJuiceRecipe.printRecipe();
     }
 }
